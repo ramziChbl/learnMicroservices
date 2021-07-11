48ccdf70550b591f0bf83fc6423110498067bc80
ramziChbl <ramzy.chbl@gmail.com>
2021-07-11 11:34:51 -0400

    added pokemonSearch function and removed old functions

--- src/db/app.py
+++ src/db/app.py
@@ -16,63 +16,23 @@ def functional():
 def pokedex():
 	return 'wow'
 
-
-@app.route('/api/pokedex/<int:generation>/<int:pokemonNumber>')
-def pokemon(generation, pokemonNumber):
-	if generation not in range(1,9):
-		return 'Wrong generation'
-	print((generation, pokemonNumber))
-	print('data/gen{}.csv'.format(int(generation)))
-	with open('data/gen{}.csv'.format(int(generation))) as file:
-		print('IM in')
-		reader = csv.DictReader(file)
-		print(reader)
-		for row in reader:
-			print(row)
-			if int(row['Num']) == pokemonNumber:
-				print(row)
-				return json.dumps(row)
-	return 'not found'
-
-@app.route('/api/pokedex/<int:generation>/<int:pokemonNumber>/<string:field>')
-def field(generation, pokemonNumber, field):
-	if generation not in range(1,9):
-		return 'Wrong generation'
-	print((generation, pokemonNumber))
-	print('data/gen{}.csv'.format(int(generation)))
-	with open('data/gen{}.csv'.format(int(generation))) as file:
-		print('IM in')
-		reader = csv.DictReader(file)
-		print(reader)
-		for row in reader:
-			print(row)
-			if int(row['Num']) == pokemonNumber:
-				if row[field]:
-					return json.dumps(row[field])
-				break
-	return 'not found'
-'''
 @app.route('/api/pokedex/<string:pokemonName>')
-def field(pokemonName):
-	print((generation, pokemonNumber))
-	print('data/gen{}.csv'.format(int(generation)))
-	with open('data/gen{}.csv'.format(int(generation))) as file:
-		print('IM in')
+def pokemonSearch(pokemonName):
+	pokedexFilepath = os.path.join(DIRNAME, 'data/pokedex.csv')
+	
+	# Sanitize input
+	pokemonName = pokemonName.lower()
+	with open(pokedexFilepath, 'r', newline='') as file:
 		reader = csv.DictReader(file)
-		print(reader)
-		for row in reader:
-			print(row)
-			if int(row['Num']) == pokemonNumber:
-				if row[field]:
-					return json.dumps(row[field])
-				break
-	return 'not found'
-'''
+		result = list(filter(lambda x: (x['Name'].lower().replace(' ', '-') == pokemonName), reader))
+		if result:
+			return json.dumps(list(result))
+	return 'Pokemon {} not found'.format(pokemonName)
+
 @app.route('/api/pokedex/all')
 def all():
-	filepath = os.path.join(DIRNAME, 'data/pokedex.csv')
-	with open(filepath, 'r') as file:
+	pokedexFilepath = os.path.join(DIRNAME, 'data/pokedex.csv')
+	with open(pokedexFilepath, 'r', newline='') as file:
 		reader = csv.DictReader(file)
 		return json.dumps(list(reader))
-
-	return 'pokedex not found'
+	return 'Pokedex not found'
