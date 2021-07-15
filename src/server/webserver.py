66dec800e7a74eed49867ceb39c36dae2e10dd84
ramziChbl <ramzy.chbl@gmail.com>
2021-07-14 20:38:39 -0400

    webserver can now render data from db

--- src/server/webserver.py
+++ src/server/webserver.py
@@ -1,10 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 import os, requests, json
 from requests import HTTPError
-import pprint
-from collections import OrderedDict
-
-FIELDS = ['Num', 'Image url', 'Name', 'Type1', 'Type2','Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']
 
 DIRNAME = os.path.dirname(__file__)
 #DB_HOSTNAME = 'db'
@@ -19,21 +15,22 @@ def main():
     #response = requests.get(url)
     try:
         response = requests.get(url)
-        print('conncection established')
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
-        decodedResponse = json.loads(response.text)
-        print(len(decodedResponse))
-        #print(decodedResponse)
-        pprint.pprint(decodedResponse)
+        print('connection to db established')
+
+        pokedex = json.loads(response.text)
+
+        tableFields = pokedex[0].keys()
+
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')  # Python 3.6
     except Exception as err:
         print(f'Other error occurred: {err}')  # Python 3.6
     else:
         print("Status:", response.status_code)
-    #return render_template('pokedex.html')
-    return 'lol'
+
+    return render_template('pokedex.html', tableFields = tableFields, pokedex=pokedex)
 
 if __name__ == "__main__":
     app.run(port=5001)
